22ed534990d845e644eeb7128b1794b3242b5045
Kevin J <kevin@Kevins-MacBook-Pro.local>
2023-01-24 22:30:06 +0900

    django course 45 commit

--- projectapp/models.py
+++ projectapp/models.py
@@ -1,13 +1,12 @@
-from django.contrib.auth.models import User
 from django.db import models
-
 # Create your models here.
+
 class Project(models.Model):
+    image = models.ImageField(upload_to='project/', null=False)
+    title = models.CharField(max_length=20, null=False)
+    description = models.CharField(max_length=200, null=True)
 
-    writer = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, related_name='project')
+    created_at = models.DateTimeField(auto_now=True)
 
-    title = models.CharField(max_length=100, null=False)
-    description = models.TextField(null=True)
-    image = models.ImageField(upload_to='project/', null=True)
-
-    created_at = models.DateField(auto_now=True)
+    def __str__(self):
+        return f'{self.pk} : {self.title}'
